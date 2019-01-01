4f8de4ae4e96854cca8802cff9fe2d78d60143f3
Adam Holt <holtsoftware@outlook.com>
2019-01-01 15:33:23 -0700

    Added Expected Projects.

--- src/Sannel.House.SensorLogging/Startup.cs
+++ src/Sannel.House.SensorLogging/Startup.cs
@@ -1,14 +1,4 @@
-/* Copyright 2018 Sannel Software, L.L.C.
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-      http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.*/
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,13 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Sannel.House.SensorEntries
+namespace Sannel.House.SensorLogging
 {
 	public class Startup
 	{
@@ -37,24 +26,6 @@ namespace Sannel.House.SensorEntries
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-			services.AddDbContextPool<DbContext>(o =>
-			{
-				switch (Configuration["Db:Provider"])
-				{
-					case "mysql":
-						//o.ConfigureMySql(Configuration["Db:ConnectionString"]);
-						break;
-					case "sqlserver":
-						//o.ConfigureSqlServer(Configuration["Db:ConnectionString"]);
-						break;
-					case "sqlite":
-					default:
-						//o.ConfigureSqlite(Configuration["Db:ConnectionString"]);
-						break;
-				}
-			});
-
-
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
